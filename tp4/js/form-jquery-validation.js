c634fa20ba36984a58d271b5c33aa209660a87e2
Massinissa-Bendali <44147103+Massinissa-Bendali@users.noreply.github.com>
2019-01-03 15:01:27 +0100

    finalisé

--- tp4/js/form-jquery-validation.js
+++ tp4/js/form-jquery-validation.js
@@ -49,17 +49,17 @@ $(document).ready(function () {
 
             var inputNom= document.getElementById("name");
             var inputPrenom= document.getElementById("firstname");
-            var inputDn= document.getElementById("birth");
+            var inputDate= document.getElementById("birth");
             var inputAdresse= document.getElementById("adresse");
             var inputEmail= document.getElementById("mail");
             
-            if($("#name").val() !== "" && $("#firstname").val() !== "" && $("#birth").val() !== "" && $("#adresse").val() !== "" && $("#mail").val() !== "" ){
+            if($("#name").val().length >= 5   && $("#firstname").val().length >= 5 && $("#birth").val().length >= 5 && $("#adresse").val().length >= 5 && $("#mail").val().length >= 5 ){
             
             //stockage local de valeurs saisies 
 
             localStorage.setItem("name", inputNom.value);
             localStorage.setItem("firstname", inputPrenom.value);
-            localStorage.setItem("birth", inputDn.value);
+            localStorage.setItem("birth", inputDate.value);
             localStorage.setItem("adresse", inputAdresse.value);
             localStorage.setItem("mail", inputEmail.value);
     
@@ -72,14 +72,16 @@ $(document).ready(function () {
            
            document.querySelector("table tbody").innerHTML = document.querySelector("table tbody")
            .innerHTML +'<tr><td>'+localStorage.getItem("name")+'</td><td>'+localStorage.getItem("firstname") +'</td><td>'+localStorage.getItem("birth")+'</td><td><a href="https://maps.google.com/maps?q=' +localStorage.getItem("adresse")+'">'+localStorage.getItem("adresse") +'</a></td><td><a href=mailto:>'+localStorage.getItem("mail")+'</a></td>';
-           }
+          
           
            document.querySelector("table tbody").innerHTML = document.querySelector("table tbody")
            .innerHTML +'<tr><td>'+contactStore.getList.name.val()+
            '</td><td>'+localStorage.getItem("firstname") +
            '</td><td>'+localStorage.getItem("birth")+'</td><td><a href="https://maps.google.com/maps?q=' +
            localStorage.getItem("adresse")+'">'+localStorage.getItem("adresse") +'</a></td><td><a href=mailto:>'+localStorage.getItem("mail")+'</a></td>';
-           
+        } else {
+            $('#myModal').modal("show");
+        }
          
            
           
