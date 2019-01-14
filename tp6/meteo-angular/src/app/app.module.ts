fc552a1928af015f50b50e98e48ec502f1a94516
Massinissa-Bendali <44147103+Massinissa-Bendali@users.noreply.github.com>
2019-01-14 17:01:10 +0100

    essai

---
+++ tp6/meteo-angular/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MeteoComponent } from './meteo/meteo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MeteoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+
